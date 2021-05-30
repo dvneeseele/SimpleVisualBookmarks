e372c77033d21c504abf77599bf3ec3a5227c921
dvneeseele <dvneeseele@gmail.com>
2021-05-30 16:12:55 -0500

    - basic frontend setup

--- svb_ui.py
+++ svb_ui.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-from PyQt5.QtWidgets import (QMainWindow, QTabBar, QMenuBar, QSplitter, QWidget, QGridLayout, QTreeView, QStackedWidget, QAction)
+from PyQt5.QtWidgets import (QMainWindow, QTabBar, QMenuBar, QSplitter, QWidget, QGridLayout, QTreeView, QStackedWidget, QAction, QToolBar)
 
 
 
@@ -14,3 +14,39 @@ class VisualBookmarksUI(object):
         super(VisualBookmarksUI).__init__()
 
 
+    def setupUI(self, MainWindow):
+
+        # menubar
+
+        menubar = MainWindow.menuBar()
+
+        self.mb_file = menubar.addMenu("File")
+        self.mb_edit = menubar.addMenu("Edit")
+        self.mb_view = menubar.addMenu("View")
+        self.mb_help = menubar.addMenu("Help")
+
+
+        # Toolbar
+
+        self.toolbar = QToolBar(MainWindow)
+        self.toolbar.setIconSize(QSize(40, 40))
+
+        MainWindow.addToolBar(self.toolbar)
+
+
+
+        self.centralWidget = QWidget(MainWindow)
+        self.centralWidget.setObjectName('central')
+
+        self.splitter = QSplitter(self.centralWidget)
+        self.splitter.setOrientation(Qt.Horizontal)
+        self.splitter.setStretchFactor(0, 25)
+        self.splitter.setStretchFactor(1, 75)
+
+
+        self.sidebar = QTreeView(self.splitter)
+        self.sidebar.setObjectName('sidebartree')
+        self.sidebar.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+
+
+        
