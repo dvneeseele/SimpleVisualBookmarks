7bd6bb85e1f3a2f8711292ed182413fd53fdf0ea
dvneeseele <dvneeseele@gmail.com>
2021-06-05 16:15:16 -0500

    - page.py class changed to subclassed QTableWidget - Basic table properties set

--- page.py
+++ page.py
@@ -1,28 +1,44 @@
 
 from bmarkLabel import bookmarkLabel
-from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5.QtWidgets import QAbstractItemView, QTableWidget, QWidget, QGridLayout
+from PyQt5.QtCore import Qt
 
 
+# class CatagoryPage(QWidget):
+#     def __init__(self):
+#         super().__init__()
 
-class CatagoryPage(QWidget):
+
+#         self.grid = QGridLayout()
+
+#         # row 1
+#         self.grid.addWidget(bookmarkLabel(), 1, 1)
+#         self.grid.addWidget(bookmarkLabel(), 1, 2)
+#         self.grid.addWidget(bookmarkLabel(), 1, 3)
+
+#         # row 2
+#         self.grid.addWidget(bookmarkLabel(), 2, 1)
+#         self.grid.addWidget(bookmarkLabel(), 2, 2)
+#         self.grid.addWidget(bookmarkLabel(), 2, 3)
+
+#         self.setLayout(self.grid)
+
+
+class CatagoryPage(QTableWidget):
     def __init__(self):
         super().__init__()
 
+        self.setObjectName('catagory-content-table')
 
-        self.grid = QGridLayout()
-
-        # row 1
-        self.grid.addWidget(bookmarkLabel(), 1, 1)
-        self.grid.addWidget(bookmarkLabel(), 1, 2)
-        self.grid.addWidget(bookmarkLabel(), 1, 3)
-
-        # row 2
-        self.grid.addWidget(bookmarkLabel(), 2, 1)
-        self.grid.addWidget(bookmarkLabel(), 2, 2)
-        self.grid.addWidget(bookmarkLabel(), 2, 3)
-
-        self.setLayout(self.grid)
-
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setSelectionMode(QAbstractItemView.MultiSelection)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.setFont(QFont('Arial', 14))
+        self.setWordWrap(False)
         
+        self.setColumnCount(4)
+        self.verticalHeader().setDefaultSectionSize(140)
+        self.horizontalHeader().setDefaultSectionSize(120)
 
 
