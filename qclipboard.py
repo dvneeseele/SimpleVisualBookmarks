e3543069120dba73946c5ea2596cdf3857060cbf
dvneeseele <dvneeseele@gmail.com>
2021-06-08 20:54:11 -0500

    created test file for Qt interaction with system clipboard

---
+++ qclipboard.py
@@ -0,0 +1,19 @@
+#############################################################################
+# dvneeseele
+#############################################################################
+
+import os
+import sys
+from PyQt5.Qt import QApllication, QClipboard 
+from PyQt5.QtWidgets import QWidget, QListWidget, QVBoxLayout
+
+
+
+class clipboardGrabber(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+        
+
+
