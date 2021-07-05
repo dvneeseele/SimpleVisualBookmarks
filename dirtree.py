b95df8ca7afeb1260f84a2a5346a55d2b170780a
dvneeseele <dvneeseele@gmail.com>
2021-07-05 12:35:50 -0500

    - dirtree.py to subclass the QTreeWidget

---
+++ dirtree.py
@@ -0,0 +1,16 @@
+#############################################################################
+# dvneeseele
+#############################################################################
+
+import os
+import sys
+import json
+from PyQt5.Qt import Qt
+from PyQt5.QtWidgets import QTreeWidget ,QAbstractItemView, QTreeWidgetItem
+
+
+class directories(QTreeWidget):
+    def __init__(self):
+        super().__init__()
+
+
