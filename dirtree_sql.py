0795e388ef70cd5ff4a34ed05fbee7ba07094471
dvneeseele <dvneeseele@gmail.com>
2021-07-13 23:47:18 -0500

    added dbLoad function and new testfile for qtreewidget sqlite storage

---
+++ dirtree_sql.py
@@ -0,0 +1,90 @@
+#############################################################################
+# dvneeseele
+#############################################################################
+
+import os
+import sys
+import sqlite3
+import string
+from random import choice
+from PyQt5 import QtCore
+from PyQt5.Qt import Qt
+from PyQt5.QtWidgets import QTreeWidget, QAbstractItemView, QTreeWidgetItem, QMenu, QApplication
+from PyQt5.QtCore import *
+
+class directories(QTreeWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.treedict = {}
+
+        self.setMouseTracking(True)
+        self.setHeaderHidden(True)
+        self.setDragEnabled(True)
+        self.setDragDropMode(QAbstractItemView.InternalMove)
+        self.setAcceptDrops(True)
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.directoryMenu)
+
+    def directoryMenu(self, event):
+        self.dirContextMenu = QMenu()
+
+        addFolder = self.dirContextMenu.addAction('Add Folder')
+        addRootFolder = self.dirContextMenu.addAction('Add Root Folder')
+        deleteFolder = self.dirContextMenu.addAction('Delete Folder')
+        renameFolder = self.dirContextMenu.addAction('Rename Folder')
+
+        action = self.dirContextMenu.exec_(self.mapToGlobal(event))
+
+        if action == addFolder:
+            current = self.currentItem()
+            #parentnode = current.parent()
+
+            addSubDir = QTreeWidgetItem()
+            addSubDir.setText(0, "Test...")
+            if not self.currentIndex().parent().isValid():
+                newroot = QTreeWidgetItem(self)
+                newroot.setText(0, "This is a new root level directory")
+                newroot.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable)
+                d = {"parent" : "", "name" : newroot.text(), "tags" : []}
+                with open("./treejson/{}.json".format(newroot.text()), 'w') as f:
+                    d.dump()
+            else:
+                current.addChild(addSubDir)
+                parent = current.parent().text()
+                # update the json
+                # if os.path.exists() then append the item to the json file with the filename of the QTreeWidgetItem.text()
+                if os.path.exists("./treejson/{}.json".format(parent), 'w'):
+                    # append to the existing json file
+                    pass
+                else:
+                    pass
+                    # parse skeleton json file add to it and do another with open to save it and close the read
+
+        if action == addRootFolder:
+            newroot = QTreeWidgetItem(self)
+            newroot.setText(0, "New Root Folder")
+            newroot.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable)
+
+        if action == deleteFolder:
+            try:
+                current = self.currentItem()
+                parent = current.parent()
+                current.removeChild(current)
+            except Exception:
+                print(Exception)
+
+        if action == renameFolder:
+            current = self.currentItem()
+            current.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable)
+            currentcol = self.currentIndex()
+            self.editItem(current, currentcol.column())
+
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = directories()
+#     window.show()
+#     sys.exit(app.exec_())
--- svb.py
+++ svb.py
@@ -12,7 +12,7 @@ from svb_ui import VisualBookmarksUI
 from dirtree import directories
 
 from PyQt5 import Qt
-from PyQt5.QtWidgets import QMainWindow, QTreeWidgetItem, QMenu
+from PyQt5.QtWidgets import QMainWindow, QTreeWidgetItem, QMenu, QMessageBox
 from PyQt5.QtCore import *
 
 
@@ -43,6 +43,58 @@ class VisualBookmarksApp(VisualBookmarksUI):
 
         #self.mainWindow.closeEvent = self.closeEvent
 
-        # call load function
-        # self.dbLoad()
+        #call load function
+        self.dbLoad()
 
+
+
+
+
+
+    def dbLoad(self):
+
+        if os.path.exists('svb.sqlite'):
+
+            conn = sqlite3.connect('svb.sqlite')
+            cursor = conn.cursor()
+        else:
+            dbFileErrorMsg = QMessageBox.question(self.mainWindow, 'Error - Database Not Found', 'svb.sqlite db file was not found in the current directory press ok and a new one will be created.', QMessageBox.Ok, QMessageBox.Cancel)
+
+        if dbFileErrorMsg == QMessageBox.Ok:
+            self.newDB()
+            conn = sqlite3.connect('svb.sqlite')
+            cursor = conn.cursor()
+        else:
+            sys.exit()
+
+        # Query to get the treewidget nodes to be loaded
+
+        # Another Query to get the corresponding table for the treewidgetitem
+
+
+
+
+
+
+
+
+
+        # for row_num, row_data in enumerate(res):
+        #     self.watchListTable.insertRow(row_num)
+        #     for column_number, column_data in enumerate(row_data):
+        #         item = str(column_data)
+        #         if column_number == 0:
+        #             self.tableLabel = QLabel()
+        #             self.tableLabel.setScaledContents(True)
+        #             pixmap = QPixmap()
+        #             pixmap.loadFromData(column_data)
+        #             self.tableLabel.setPixmap(pixmap)
+
+        #             self.watchListTable.setCellWidget(row_num, column_number, self.tableLabel)
+        #         else:
+        #             self.watchListTable.setItem(row_num, column_number, QTableWidgetItem(column_data))
+        #     self.watchListTable.verticalHeader().setDefaultSectionSize(140)
+        #     self.watchListTable.horizontalHeader().setDefaultSectionSize(120)
+
+
+        # conn.close()
