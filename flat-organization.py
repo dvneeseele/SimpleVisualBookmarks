f506660236bb0b78c65242eeff2d953b9ca1f324
dvneeseele <dvneeseele@gmail.com>
2021-08-14 15:32:58 -0500

    basic context menu setup. add, delete, and rename actions

--- flat-organization.py
+++ flat-organization.py
@@ -9,7 +9,7 @@ import string
 from random import choice
 from PyQt5 import QtCore
 from PyQt5.Qt import Qt
-from PyQt5.QtWidgets import QListWidget, QTreeWidget, QAbstractItemView, QTreeWidgetItem, QMenu, QApplication
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QTreeWidget, QAbstractItemView, QTreeWidgetItem, QMenu, QApplication
 from PyQt5.QtCore import *
 
 
@@ -25,6 +25,27 @@ class Catagories(QListWidget):
         self.customContextMenuRequested.connect(self.catagoryMenu)
 
 
-    def catagoryMenu(self):
-        pass
+    def catagoryMenu(self, event):
+        self.catagoryContextMenu = QMenu()
+
+        addCatagory = self.catagoryContextMenu.addAction('Add Catagory')
+        deleteCatagory = self.catagoryContextMenu.addAction('Delete Catagory')
+        renameCatagory = self.catagoryContextMenu.addAction('Delete Catagory')
+
+        action = self.catagoryContextMenu.exec_(self.mapToGlobal(event))
+
+        if action == addCatagory:
+            catagory = QListWidgetItem()
+            catagory.setText("Test Text.....")
+            self.addItem(catagory)
+
+        if action == deleteCatagory:
+            selection = self.currentItem()
+            print(selection)
+            self.takeItem(selection)
+
+        if action == renameCatagory:
+            selection = self.currentItem()
+            self.editItem(selection)
+            
         
